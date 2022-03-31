f3ab83353a8bc6baba9c206aea12f5e62eba8309
Omar Domen <44986107+DOM00747@users.noreply.github.com>
2022-03-31 14:06:19 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import numpy as np 
+import pandas as pd 
+from flask import Flask , request , render_template
+import joblib
+
+#crating the flask app
+
+app = Flask(__name__)
+model = joblib.load("spotify-recommender.pkl")
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+
+@app.route('/predict' , methods = ['POST'])
+def predict():
+
+	int_features = [float(x) for x in request.form.values()]
+	final_features = [np.array(int_features)]
+	prediction = model.predict(final_features)
+	prediction = prediction[0]
+	print(prediction)
+
+
+	if (prediction < 0.5):
+		return render_template('index.html' , prediction_text = 'Prediction: DISLIKED')
+	
+	else:
+		return render_template('index.html', prediction_text='Prediction: LIKED')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
